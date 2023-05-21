ddc5f01c68d7371d17a9585d42d513deeb545f30
Cooktypeset <tovpyhalitva@gmail.com>
2023-05-21 21:22:28 +0300

    firstupd

---
+++ js/task-02.js
@@ -0,0 +1,20 @@
+
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const elementsEl = ingredients.map((option) => {
+  const ingredientsItemEl = document.createElement("li");
+  ingredientsItemEl.classList.add("item");
+  ingredientsItemEl.textContent = option;
+
+  return ingredientsItemEl;
+});
+const ingredientsListEl = document.querySelector("#ingredients");
+console.log(ingredientsListEl);
+ingredientsListEl.append(...elementsEl);
