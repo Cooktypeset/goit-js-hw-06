f26f1cb4460278aaedd0b17d16c7d2131e056f22
Cooktypeset <tovpyhalitva@gmail.com>
2023-05-25 19:32:51 +0300

    upgrNew

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,16 @@
-const categoriesItemEl = document.querySelectorAll(".item");
-console.log(`Number of categories: ${categoriesItemEl.length}`);
-categoriesItemEl.forEach((item) => {
-  const itemTitleEl = item.querySelector("h2").textContent;
-  console.log(`Category: ${itemTitleEl}`);
-  const itemsListEl = item.querySelectorAll("li");
-  console.log(`Elements: ${itemsListEl.length}`);
-  const listItems = item.children;
-  console.log(listItems);
-const firstListItem = item.firstElementChild;
-  console.log(firstListItem);
-  const lastListItem = item.lastElementChild;
-  console.log(lastListItem);
+const categoriesItemEl = document.querySelector('#categories').children;
+console.log('Number of categories:', categoriesItemEl.length);
+[...categoriesItemEl].forEach(itemEl => {
+  console.log('Category:', itemEl.firstElementChild.textContent);
+  console.log('Elements:', itemEl.lastElementChild.children.length);
 });
+
+
+// const categoriesItemEl = document.querySelectorAll(".item");
+// console.log(`Number of categories: ${categoriesItemEl.length}`);
+// categoriesItemEl.forEach((item) => {
+//   const itemTitleEl = item.querySelector("h2").textContent;
+//   console.log(`Category: ${itemTitleEl}`);
+//   const itemsListEl = item.querySelectorAll("li");
+//   console.log(`Elements: ${itemsListEl.length}`);
+// });
